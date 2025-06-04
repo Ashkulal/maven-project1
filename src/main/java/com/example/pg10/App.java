9072eed1d2126d290657a4bf3889bb12802e9e39
Ashkulal <akashkulal866087@example.com>
2025-06-04 09:50:46 +0530

    first commit

---
+++ src/main/java/com/example/pg10/App.java
@@ -0,0 +1,13 @@
+package com.example.pg10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
